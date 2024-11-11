96bb243ca423e4e879d6d002c40c8bdfa3daad7c
Shi Lei <shi_lei@massclouds.com>
2024-11-11 07:54:00 +0000

    add doc

--- rt_exec/src/main.rs
+++ rt_exec/src/main.rs
@@ -20,7 +20,7 @@ pub extern "Rust" fn runtime_main(cpu_id: usize, dtb: usize) {
 }
 
 pub fn init(cpu_id: usize, dtb: usize) {
-    axlog2::init("error");
+    axlog2::init("debug");
     exec::init(cpu_id, dtb);
     axtrap::init(cpu_id, dtb);
 }
